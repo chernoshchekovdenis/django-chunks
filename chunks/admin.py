e2c713786c924cd370aa985a3e87aa9908cdde01
chernoshchekovdenis <denis.chernoshchekov@djangostars.com>
2012-03-14 17:08:42 +0200

    Added TinyMCE widget to content field

--- chunks/admin.py
+++ chunks/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from models import Chunk
+from tinymce.widgets import TinyMCE
 
 class ChunkAdmin(admin.ModelAdmin):
-  list_display = ('key','description',)
-  search_fields = ('key', 'content')
+    list_display = ('key','description',)
+    search_fields = ('key', 'content')
+
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name in ['content']:
+            kwargs['widget'] = TinyMCE(attrs={'rows': 4})
+
+        return super(ChunkAdmin1, self).formfield_for_dbfield(db_field, **kwargs)
 
 admin.site.register(Chunk, ChunkAdmin)
